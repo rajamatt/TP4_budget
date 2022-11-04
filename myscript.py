d7262745499649c3104d596aba8099c813b79901
Matthew Rajala <matthew.rajala@polymtl.ca>
2022-11-04 16:30:52 -0400

    added main to script

--- myscript.py
+++ myscript.py
@@ -1,3 +1,8 @@
 import os
-os.system('git bisect start c1a4be04b972b6c17db242fc37752ad517c29402 e4cfc6f77ebbe2e23550ddab682316ab4ce1c03c')
-os.system('git bisect run python manage.py test')
+
+def bisect():
+    os.system('git bisect start c1a4be04b972b6c17db242fc37752ad517c29402 e4cfc6f77ebbe2e23550ddab682316ab4ce1c03c')
+    os.system('git bisect run python manage.py test')
+
+if __name__ == '__main__':
+    bisect()
